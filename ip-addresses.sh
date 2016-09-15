a3336b2ed2078a1242ae5999c50d96fba511ed99
Jesse Millar <hellojessemillar@gmail.com>
2016-09-15 12:42:36 -0600

    Adding tags to the ip addresses

--- ip-addresses.sh
+++ ip-addresses.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ipconfig getifaddr en0 && curl icanhazip.com
+LOCAL_ADDRESS=$(ipconfig getifaddr en0)
+EXTERNAL_ADDRESS=$(curl -s icanhazip.com)
+
+printf "Local: %s\n" $LOCAL_ADDRESS && printf "External: %s" $EXTERNAL_ADDRESS
