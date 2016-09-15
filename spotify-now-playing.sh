089c6832a10ec83cbc6e9617fb40c7ab21d811a6
Jesse Millar <hellojessemillar@gmail.com>
2016-09-15 09:19:23 -0600

    Fixing the Spotify script to be just one script

--- spotify-now-playing.sh
+++ spotify-now-playing.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-osascript ${BASH_SOURCE%/*}/spotify-now-playing.scpt
+osascript <<EOD
+  tell application "System Events"
+  	set appList to name of every process
+  	if appList does not contain "Spotify" then
+  		return ""
+  	else
+  		tell application "Spotify"
+  			if player state is not stopped then
+  				set currentTrack to current track
+  				tell currentTrack
+  					set trackName to name
+  					set trackArtist to artist
+  				end tell
+  				if player state is playing then
+  					return trackName & return & trackArtist
+  				else
+  					return trackName & " (paused)" & return & trackArtist
+  				end if
+  			else
+  				return ""
+  			end if
+  		end tell
+  	end if
+  end tell
+EOD
